4c0354d36e60bbda5d6546b796169cc5bf5d3bb4
yesmeanshhs <149229403+yesmeanshhs@users.noreply.github.com>
2024-04-10 21:06:48 +0700

    Update LuaStacksModule.lua

--- LuaStacksModule.lua
+++ LuaStacksModule.lua
@@ -1,25 +1,100 @@
-Stack = {}
-Stack.__index = Stack
-function Stack.new() return setmetatable({}, Stack) end
+-- Stack principle in Roblox Lua (Exploits)
+-- This follows a basic of best performance wise way of creating undo redo system
+-- With this, you can load this github and start making it act like a module script
 
-function Stack:push(input)
-	self[#self+1] = input
+local Stacks = {}
+Stacks.__index = Table
+Stacks.ConvertToString = function(self)
+local Strings = {}
+for _, v in ipairs(self.StackData) do
+table.insert(Strings,tostring(v))
+end
+return "{"..table.concat(Strings,", ").."}"
 end
 
-function Stack:pop()
-	assert(#self > 0, "Stack underflow")
-	local output = self[#self]
-	self[#self] = nil
-	return output
+function Stacks.New()
+return setmetatable({StackData = {},StackAmount = 0},Stacks)
 end
 
-function Stack:size()
-	return #self
+function Stacks:Push(...)
+local RetrievedArgument = select("#",...)
+if RetrievedArgument == 0 then
+error("A Stacks Function tried to push with an empty argument! (Error_ArgumentParam_nil)")
+end
+for IndexLoop = 1, RetrievedArgument do
+local ValueTuple = select(IndexLoop,...)
+self.StackAmount += 1
+self.StackData[self.StackAmount] = ValueTuple
+end
 end
 
-function Stack:iterator()
-  return function()
-    return self:pop()
+function Stacks:Pop()
+if self.StackAmount == 0 then
+return nil
+end
+if self.StackData[self.StackAmount] then
+local ItemStackData = self.StackData[self.StackAmount]
+self.StackData[self.StackAmount] = nil
+self.StackAmount -= 1
+return ItemStackData
+end
+return nil
 end
 
-return Stack
+function Stacks:Peek()
+if self.StackAmount == 0 then
+return nil
+end
+if self.StackData[self.StackAmount] then
+return self.StackData[self.StackAmount]
+end
+return nil
+end
+
+function Stacks:Clear()
+self.StackData = {}
+self.StackAmount = 0
+end
+
+function Stacks:Empty()
+return self.StackAmount == 0
+end
+
+function Stacks:Size()
+return self.StackAmount
+end
+
+-- This will be providing a way of creating TAS in roblox
+
+function Stacks:Iterator()
+local Index = 0
+if self.StackAmount == 0 then
+return function() end
+end
+if Index > self.StackAmount then
+return function() end
+end
+return function()
+if Index < self.StackAmount then
+local IndexValue = self.StackData[Index]
+Index += 1
+return IndexValue
+end
+end
+end
+
+function Stacks:ReverseIterator()
+local Index = self.StackAmount
+if Index == 0 then
+return function() end
+end
+return function()
+if Index > 0 then
+local IndexValue = self.StackData[Index]
+Index -= 1
+return IndexValue
+end
+end
+end
+
+return Stacks
